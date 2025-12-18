c67b9e55d6ea673fc18ba4fed2a29c385ea73340
shivam atoliya <143529285+shivamdhakad14@users.noreply.github.com>
2025-12-18 11:40:07 +0530

    add new service

---
+++ service-registry/src/test/java/com/digital_wallet/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_wallet.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
